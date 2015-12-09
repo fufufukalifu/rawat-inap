857378b86ed681d6c2f8bc992cdd5412ba09ba9a
Anggiyasti <putrianggiyasti@gmail.com>
2015-12-09 20:46:52 +0700

    Coba commit

--- resources/views/pengunjung/form-pengunjung.blade.php
+++ resources/views/pengunjung/form-pengunjung.blade.php
@@ -17,6 +17,8 @@
   consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
   cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
   proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+  test
 	<div class="container">
 		<div class="col-md-6">
 			<div class="row mt">
