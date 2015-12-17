2ee61ea36969196700a9df9e80eefe48a5b235c5
didikkurniawan <didik.kurniawan@mail.unpas.ac.id>
2015-12-17 18:17:09 +0700

    firsh
    saya bkin daftar pengunjung HEHE

---
+++ resources/views/Pasien/form-pendaftaran.blade.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="container">
+		<div class="col-md-6">
+			<div class="row mt">
+				<div class="col-lg-12">
+						<div class="row mt">
+				<div class="col-lg-12">
+					<div class="form-panel">
+					 <h6 class="mb"><i class="fa fa-male"></i> Data Pengunjung</h6>
+							<form class="form-horizontal style-form" method="get">
+				       <div class="form-group">
+				        <div class="col-md-10">
+				         <input type="text" placeholder="Nama Pasien atau Nama Penanggung Jawab" class="form-control">
+				        </div>
+				       </div>
+       
+				       <div class="form-group">
+				        <div class="col-md-10">
+				        <label class="sr-only">No Identitas</label>
+				         <input type="text" placeholder="contoh :123040031" class="form-control">
+				        </div>
+				       </div>
+       
+			       <div class="form-group">
+			        <div class="col-md-10">
+			        <label class="sr-only">Nama Lengkap</label>
+			         <input type="text" placeholder="contoh :Jajang Miharja" class="form-control">
+			        </div>
+			       </div>
+				</div>
+  			</div>
+		</div>
+	</div>
+
+</body>
+</html>
